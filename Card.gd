1d52a5e16fcb2fd6fdd4f761f02b6fde6a95e322
Natale Toscano <kickalliance195@gmail.com>
2020-05-27 23:27:22 -0400

    Allow card to detect other cards in vicinity

--- Card.gd
+++ Card.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends Area2D
 
 enum Suit {
 	HEARTS,
@@ -24,22 +24,40 @@ enum Value {
 }
 
 export var suit = Suit.CLUBS
-
 export var value = Value.ACE
+var wasPressed = false
+var cursorPositionOffset: Vector2
+var cardsInVicinity: Array
 
 func _ready():
 	pass
 
-
 func _on_Card_mouse_entered():
-	$Sprite.modulate = Color(0,0,0)
+	$Sprite.modulate = Color(0.9,0.9,0.9)
 	
-
-
 
 func _on_Card_mouse_exited():
 	$Sprite.modulate = Color(1,1,1)
 
-
 func _on_Card_input_event(viewport, event, shape_idx):
-	print(event)
+	if event.is_pressed() && !self.wasPressed:
+		#clicked
+		self.cursorPositionOffset = viewport.get_mouse_position() - self.position
+		wasPressed = true
+	if(wasPressed && !Input.is_mouse_button_pressed(1)):
+		wasPressed = false
+		print(cardsInVicinity)
+
+func _process(delta):
+	if wasPressed:
+		self.position = get_viewport().get_mouse_position() - cursorPositionOffset
+
+
+func _on_Card_area_entered(area):
+	if(wasPressed && !area in self.cardsInVicinity):
+		self.cardsInVicinity.append(area)
+
+
+func _on_Card_area_exited(area):
+	if(wasPressed):
+		cardsInVicinity.remove(cardsInVicinity.find(area))
